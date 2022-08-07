a08d1e6871f6413f43f7d95a71d151240246b703
Marquis Kurt <software@marquiskurt.net>
2022-08-07 16:39:18 -0400

    :sparkles: Add CGPoint.manhattanDistance

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# CranberrySprite
+# Cranberry Sprite
 
-A description of this package.
+An extended utility package for SpriteKit.
+
+## Overview
+
+**Cranberry Sprite** is a utility package born from the original GBMKUtils package to add
+extra functionality and quality-of-life updates to SpriteKit.
+
+Some features include the following:
+
+- Protocols for parsing SKTileMapNodes to create SKSpriteNodes from them.
+- Extensions to CGPoint and CGVector to get distances.
+
+## Getting started
+
+Cranberry Sprite is a Swift package for the Swift Package Manager and can be added through
+Xcode.
+
+### Build from source
+
+**Required Tools**:
+
+- Xcode 13 or later
+- macOS 12.0 or later
+
+Clone the repository using `git clone` or `gh repo clone` then run `swift build` in the root
+of the project.
--- Sources/CranberrySprite/Core Graphics/CGPointExtension.swift
+++ Sources/CranberrySprite/Core Graphics/CGPointExtension.swift
@@ -24,4 +24,19 @@ extension CGPoint {
     public func distance(between point: CGPoint) -> CGFloat {
         return CGPoint.distance(from: point, to: self)
     }
+
+    /// Returns the Manhattan or city-block distance between two points.
+    /// - Parameter first: The starting CGPoint
+    /// - Parameter second: The destination CGPoint
+    /// - Returns: A CGFloat rerpesenting the Manhattan distance between the two points.
+    static func manhattanDistance(from first: CGPoint, to second: CGPoint) -> CGFloat {
+        return abs(first.x - second.x) + abs(first.y - second.y)
+    }
+
+    /// Returns the Manhattan or city-block distance between two points.
+    /// - Parameter point: The destination CGPoint from itself.
+    /// - Returns: A CGFloat rerpesenting the Manhattan distance between the two points.
+    func manhattanDistance(to point: CGPoint) -> CGFloat {
+        return CGPoint.manhattanDistance(from: self, to: point)
+    }
 }
