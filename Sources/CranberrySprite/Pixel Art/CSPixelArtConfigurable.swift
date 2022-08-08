261384ae016240deb3772a692c5ba2f94bb23e36
Marquis Kurt <software@marquiskurt.net>
2022-08-08 13:36:58 -0400

    :recycle: Change how `animated` operates
    The animated method now includes a format parameter that lets
    developers describe the format of the sprite's string. A default
    implementation is provided as well.

---
+++ Sources/CranberrySprite/Pixel Art/CSPixelArtConfigurable.swift
@@ -0,0 +1,21 @@
+//
+//  CSPixelArtConfigurable.swift
+//  
+//
+//  Created by Marquis Kurt on 8/8/22.
+//
+
+import SpriteKit
+
+/// A protocol that indicates an object is capable of configuring its textures for pixel art.
+public protocol CSPixelArtConfigurable {
+    /// Configures the current sprite so that its texture renders pixel art correctly.
+    func configureForPixelArt()
+}
+
+
+extension SKSpriteNode: CSPixelArtConfigurable {
+    public func configureForPixelArt() {
+        texture?.filteringMode = .nearest
+    }
+}
