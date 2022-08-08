a30902cf122bd046f5a181cdc3035b08ac214e10
Marquis Kurt <software@marquiskurt.net>
2022-08-08 14:31:24 -0400

    :sparkles: Add CSActionSequenceRunnable

---
+++ Sources/CranberrySprite/Actions/CSActionBuilder.swift
@@ -0,0 +1,17 @@
+//
+//  CSActionBuilder.swift
+//  
+//
+//  Created by Marquis Kurt on 8/8/22.
+//
+
+import SpriteKit
+
+/// A struct that can build a series of SKActions.
+@resultBuilder
+public struct CSActionBuilder {
+    /// Builds the respective list of SKActions to run in sequence.
+    public static func buildBlock(_ components: SKAction...) -> [SKAction] {
+        components
+    }
+}
---
+++ Sources/CranberrySprite/Actions/CSActionSequenceRunnable.swift
@@ -0,0 +1,29 @@
+//
+//  CSActionSequenceRunnable.swift
+//  
+//
+//  Created by Marquis Kurt on 8/8/22.
+//
+
+import SpriteKit
+
+/// A protocol that indicates an object is capable of running a sequence of actions.
+public protocol CSActionSequenceRunnable {
+    /// Run a sequence of SKActions.
+    /// - Parameter sequence: The array of actions that will be run in sequence.
+    func runSequence(_ sequence: [SKAction])
+
+    /// Run a sequence of SKActions.
+    /// - Parameter builder: A closure that returns an array of actions that will be run in sequence.
+    func runSequence(@CSActionBuilder builder: () -> [SKAction])
+}
+
+extension SKNode: CSActionSequenceRunnable {
+    public func runSequence(_ sequence: [SKAction]) {
+        self.run(.sequence(sequence))
+    }
+
+    public func runSequence(builder: () -> [SKAction]) {
+        self.run(.sequence(builder()))
+    }
+}
