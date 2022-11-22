4c59be7bd5f79c914b62fb76573e6d97a828c1c0
Marquis Kurt <software@marquiskurt.net>
2022-11-22 16:50:26 -0500

    :art: Move code around
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- Sources/CranberrySprite/Actions/CSActionBuilder.swift
+++ Sources/CranberrySprite/Actions/CSActionBuilder.swift
@@ -10,6 +10,10 @@ import SpriteKit
 /// A result builder capable of building an SKAction sequence.
 @resultBuilder
 public enum CSActionBuilder {
+    public static func buildArray(_ components: [[SKAction]]) -> [SKAction] {
+        components.flatMap { $0 }
+    }
+
     public static func buildBlock(_ components: [SKAction]...) -> [SKAction] {
         components.flatMap { $0 }
     }
